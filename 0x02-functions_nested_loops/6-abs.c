f4b58703ddb46302048c951cb2234a770fb1db0a
aaboundi <aadjaboundi@gmail.com>
2022-03-15 12:53:53 -0700

    Task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - check for alphabetic number n
+ * @n: The character to check
+ *Description : using the _abs function
+ *Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+		return (-n);
+}
