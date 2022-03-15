5d4579f8f1b3f5cbf0d1568ac5a60bc7d6b88ff1
aaboundi <aadjaboundi@gmail.com>
2022-03-15 08:13:45 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <putchar.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return : Alway 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+
+	return (0);
+}
