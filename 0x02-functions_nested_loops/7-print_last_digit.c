605325c12fb8df39961061c97faf327cc92a3693
aaboundi <aadjaboundi@gmail.com>
2022-03-15 13:01:19 -0700

    Task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check for alphabetic number n
+ * @n: The character to check
+ *Description : using the print_last_digit function
+ *Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
