cbade05731db902b98d3caac213fb3885d973036
aaboundi <aadjaboundi@gmail.com>
2022-03-21 07:49:12 -0700

    PAS1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of the variable
+ * the pointer points to 98
+ * @n: pointer to the variable to update
+ * Description : using the reset_to_98 function
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
