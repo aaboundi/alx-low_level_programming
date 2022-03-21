180cb0c3a34f18da015d3118029b87a4808f52bf
aaboundi <aadjaboundi@gmail.com>
2022-03-21 08:32:07 -0700

    PAS5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse followed by a new line
+ * @s: string to evaluate
+ * Description : using the print_rev function
+ */
+void print_rev(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = i;
+
+	for (j = k - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
