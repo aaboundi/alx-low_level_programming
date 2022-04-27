d37c6a9b7905edcf927be60641ee4f9144340c99
aaboundi <aadja@gmail.com>
2022-04-27 02:03:22 -0700

    CLM6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,7 +6,7 @@
  * Return: the data inside the elements that was deleted,
  * or 0 if the list is empty
  */
-int pop_listint(listint_t *head)
+int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int num;
