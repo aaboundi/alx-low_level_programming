b163fd3e09d5a0c12d510f1f4d650e2acad7da92
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-01 13:13:31 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,26 +1,53 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array of the hash table
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * Return: Pointer to the newly created hash table
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
-hash_table_t *hash_table_create(unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *ht;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (size == 0)
-		return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
-		return (NULL);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-	if (ht->array == NULL)
-		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 
-	return (ht);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
 }
