516313bf89973935a711a6d3112e1b51c3028fa0
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-01 13:17:02 +0000

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,26 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array of the hash table
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
  *
- * Return: Pointer to the newly created hash table
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
  */
-hash_table_t *hash_table_create(unsigned long int size)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_table_t *ht;
+	hash_node_t *node;
+	unsigned long int index;
 
-	if (size == 0)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-	if (ht->array == NULL)
-		return (NULL);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-	return (ht);
+	return ((node == NULL) ? NULL : node->value);
 }
